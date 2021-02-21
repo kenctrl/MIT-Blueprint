52fd5cd87e1f1627e3b5452505b53cbdbb428706
kenctrl <kennethkchoi22@gmail.com>
2021-02-21 09:11:26 -0500

    9:11

---
+++ MIT Blueprint/HomeViewController.swift
@@ -0,0 +1,48 @@
+//
+//  HomeViewController.swift
+//  MIT Blueprint
+//
+//  Created by Kenneth Choi on 2/21/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController{
+    
+    let myLabel = UILabel()
+    
+    override func viewDidLoad() {
+        
+        self.view.addSubview(myLabel)
+        myLabel.backgroundColor = .blue
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            myLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            myLabel.heightAnchor.constraint(equalToConstant: 50),
+            myLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50),
+            myLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -50)
+        ])
+    }
+}
+
+import SwiftUI
+// This struct from SwiftUI shows us the content preview in the canvas page.
+struct MainPreview: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+
+            func makeUIViewController(context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) -> UIViewController {
+                return UINavigationController(rootViewController: HomeViewController())
+            }
+
+            func updateUIViewController(_ uiViewController: MainPreview.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) {
+
+            }
+
+        }
+}
---
+++ MIT Blueprint/UpdateViewController.swift
@@ -0,0 +1,8 @@
+//
+//  UpdateViewController.swift
+//  MIT Blueprint
+//
+//  Created by Kenneth Choi on 2/21/21.
+//
+
+import Foundation
