c350015e2984e1862d97539a17f9618d5ac6cb45
kenctrl <kennethkchoi22@gmail.com>
2021-02-21 11:00:42 -0500

    First button

--- MIT Blueprint/HomeViewController.swift
+++ MIT Blueprint/HomeViewController.swift
@@ -9,23 +9,42 @@ import UIKit
 
 class HomeViewController: UIViewController{
     
-    let myLabel = UILabel()
+    let headLabel = UILabel()
+    let button00 = UIButton()
     
     override func viewDidLoad() {
         
-        self.view.addSubview(myLabel)
-        myLabel.backgroundColor = .blue
-        myLabel.text = "Hello World"
-        myLabel.textColor = .white
-        myLabel.textAlignment = .center
+        self.view.addSubview(headLabel)
+        headLabel.backgroundColor = .blue
+        headLabel.text = "Hello World"
+        headLabel.textColor = .white
+        headLabel.textAlignment = .center
         
-        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        headLabel.translatesAutoresizingMaskIntoConstraints = false
     
         NSLayoutConstraint.activate([
-            myLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 5),
-            myLabel.heightAnchor.constraint(equalToConstant: 50),
-            myLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50),
-            myLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -50)
+            headLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 5),
+            headLabel.heightAnchor.constraint(equalToConstant: 50),
+            headLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50),
+            headLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -50)
+            
+        ])
+        
+        self.view.addSubview(button00)
+        button00.backgroundColor = .blue
+        button00.setTitle("Sad", for: .normal)
+        button00.setTitle("Pressed", for: .highlighted)
+        button00.setTitleColor(.white, for: .normal)
+        button00.setTitleColor(.green, for: .highlighted)
+        button00.contentHorizontalAlignment = .center
+        
+        button00.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            button00.topAnchor.constraint(equalTo: headLabel.bottomAnchor, constant: 8),
+            button00.heightAnchor.constraint(equalToConstant: 100),
+            button00.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 5),
+            button00.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -266)
             
         ])
         
