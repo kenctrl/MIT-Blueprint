74223da82280a2f2f4d1b39410f654e81a47d696
kenctrl <kennethkchoi22@gmail.com>
2021-02-21 08:19:40 -0500

    Initial Commit

---
+++ MIT Blueprint/MIT_BlueprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIT_BlueprintApp.swift
+//  MIT Blueprint
+//
+//  Created by Kenneth Choi on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct MIT_BlueprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
